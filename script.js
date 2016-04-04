675ad61920bd10dd18a0b03316721538ae004f64
Andrew Smith-Mui <arsm800@gmail.com>
2016-04-03 21:02:21 -0400

    Created circle svgs for test data array.

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
-d3.select("body").append("p").text("New Paragraph!");
+var svg = d3.select("body").append("svg");
+
+svg.attr("width", 500)
+  .attr("height", 50);
+
+var dataset = [5, 10, 15, 20, 25];
+
+var circles = svg.selectAll("circle")
+                .data(dataset)
+                .enter()
+                .append("circle");
+
+circles.attr("cx", function(d, i) {
+  return (i * 50) + 25;
+})
+  .attr("cy", h/2)
+  .attr("r", function(d) {
+    return d;
+  });
